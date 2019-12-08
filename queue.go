9e6e6c8a7401db2b95749d95eb30f0f51f4ae582
asrealmadrid97 <asrealmadrid97@gmail.com>
2019-12-08 18:09:09 -0500

    final commit

---
+++ queue.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type queue struct{
+	elements []int
+}
+
+func (q1 *queue) enqueue(x int){
+	q1.elements = append(q1.elements, x)
+}
+
+func (q1 *queue) dequeue()int{
+	if len(q1.elements) == 0{
+		fmt.Println("queue is empty, hence returning -1")
+		return -1
+	}else{
+		val := q1.elements[0]
+		q1.elements = q1.elements[1:]
+		return val
+	}
+}
+
+func (q1 *queue) getFront()int{
+	if len(q1.elements) == 0{
+		fmt.Println("Queue is empty, hence returning -1")
+		return -1
+	}else{
+		return q1.elements[0]
+	}
+}
+
+
+func (q1 *queue) isEmpty()bool{
+	if len(q1.elements) == 0{
+		return true
+	}else{
+		return false
+	}
+}
+
+
+func main(){
+	q1 := queue{elements:[]int{}}
+	q1.enqueue(10)
+	q1.enqueue(11)
+	q1.enqueue(12)
+	q1.enqueue(13)
+	fmt.Println("Elements:",q1.elements)
+	fmt.Println("Dequeue:",q1.dequeue())
+	fmt.Println("Elements:",q1.elements)
+	fmt.Println("isEmpty:",q1.isEmpty())
+	fmt.Println("getFront:",q1.getFront())
+}
---
+++ stack.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type stack struct{
+	elements []int
+}
+
+func (s1 *stack) push(x int){
+	s1.elements = append(s1.elements, x)
+}
+
+func (s1 *stack) top()int{
+	if len(s1.elements) == 0{
+		fmt.Println("elements is empty, hence returning -1")
+		return -1
+	}
+	p := len(s1.elements)-1
+	return s1.elements[p]
+}
+
+func (s1 *stack) pop()int{
+	if len(s1.elements) == 0{
+		fmt.Println("No element to pop, hence returning -1")
+		return -1
+	}
+	p := len(s1.elements)-1
+	v := s1.elements[p]
+	s1.elements = s1.elements[:p]
+	return v
+}
+
+func (s1 *stack) isEmpty()bool{
+	if len(s1.elements) == 0{
+		return true
+	}else{
+		return false
+	}
+}
+
+func (s1 *stack) size()int{
+	return len(s1.elements)
+}
+
+func main(){
+	s1 := stack{elements : []int{}}
+	fmt.Println("Top element of stack:",s1.top())
+	fmt.Println("All elements in stack:",s1.elements)
+	s1.push(10)
+	s1.push(11)
+	s1.push(12)
+	s1.push(13)
+	fmt.Println("Top element of stack:",s1.top())
+	(s1.pop())
+	fmt.Println(s1.elements)
+	fmt.Println("Top element of stack:",s1.top())
+	fmt.Println("Size of stack:",s1.size())
+	fmt.Println(s1.isEmpty())
+}
